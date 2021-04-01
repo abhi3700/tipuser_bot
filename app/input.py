2e7a8b6f7c35a51faee38f089eaa2d09d8130c7d
Abhijit Roy <dalvath3700@gmail.com>
2021-04-01 21:08:14 +0530

    redis uri added

--- app/input.py
+++ app/input.py
@@ -1,4 +1,4 @@
 API_key = "1741811496:AAFkvltDTovbRtxNPE1oZEIM2qNPJmKBmWw"
 
 # Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
-REDIS_URL = 'redis://h:pb8cf3fe9644caf6fdf47dd3223bb7334d70058ecbaa8fb3e91ac41fac82ee3bc@ec2-99-81-89-240.eu-west-1.compute.amazonaws.com:15289'
+REDIS_URL = 'redis://:p66ce263b0d5ff1cb309cac27f09d077fb0a14c5d37ace401b82d02dbea514c15@ec2-108-128-25-66.eu-west-1.compute.amazonaws.com:25430'
