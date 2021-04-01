aeacbedcb3aa64e5cd40cb639cdb6941c3fb108d
Abhijit Roy <dalvath3700@gmail.com>
2021-04-02 01:25:41 +0530

    F:/Softwares/Git/deposit command added

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,5 @@
 import botogram
-import redis
+# import redis
 import json
 
 from input import *
@@ -11,24 +11,30 @@ bot.owner = "@abhi3700"
 
 # -------------------------------------------------------Redis DB------------------------------------------------------------------------
 # define Redis database
-r = redis.from_url(REDIS_URL)
+# r = redis.from_url(REDIS_URL)
 
-# ===================================================Share phone via keyboard===========================================================================
-
+# ===================================================Share telegram_id===========================================================================
+@bot.command("deposit")
+def deposit_command(chat, message, args):
+    """Please share your telegram id. Also Get it from @userinfobot"""
+    chat.send("Send token to tippedtipped with your unique telegram ID {chat_id} as the memo to deposit!".format(chat_id=chat.id))
 
 # ===================================================remove keyboard(s)=================================================================
-@bot.command("removekeyboard")
-def removekeyboard_command(chat, message):
-    """removes the keyboard appearing below"""
-    bot.api.call('sendMessage', {
-        'chat_id': chat.id,
-        'reply_to_message': message.id,
-        'text': 'keyboards removed.',
-        'reply_markup': json.dumps({
-            'remove_keyboard': True,
-            # This 1 parameter below is optional
-            # See https://core.telegram.org/bots/api#replykeyboardremove
-            'selective': True,
-        })
-    })
+# @bot.command("removekeyboard")
+# def removekeyboard_command(chat, message):
+#     """removes the keyboard appearing below"""
+#     bot.api.call('sendMessage', {
+#         'chat_id': chat.id,
+#         'reply_to_message': message.id,
+#         'text': 'keyboards removed.',
+#         'reply_markup': json.dumps({
+#             'remove_keyboard': True,
+#             # This 1 parameter below is optional
+#             # See https://core.telegram.org/bots/api#replykeyboardremove
+#             'selective': True,
+#         })
+#     })
 
+# ================================================MAIN===========================================================================
+if __name__ == "__main__":
+    bot.run()
